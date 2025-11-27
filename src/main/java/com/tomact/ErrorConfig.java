7feff7b55edaed27ca3e4095cb5f751a1534df6d
Md Nehal Ahmad <nehal.ahmad@paynways.com>
2025-11-27 16:09:37 +0530

    some changes

--- src/main/java/com/tomact/ErrorConfig.java
+++ src/main/java/com/tomact/ErrorConfig.java
@@ -1,23 +1,23 @@
-package com.tomact;
+// package com.tomact;
 
-import org.springframework.boot.web.servlet.support.ErrorPageFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.boot.web.servlet.support.ErrorPageFilter;
+// import org.springframework.boot.web.servlet.FilterRegistrationBean;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class ErrorConfig {
+// @Configuration
+// public class ErrorConfig {
 
-    // create the ErrorPageFilter bean so it can be injected
-    @Bean
-    public ErrorPageFilter errorPageFilter() {
-        return new ErrorPageFilter();
-    }
+//     // create the ErrorPageFilter bean so it can be injected
+//     @Bean
+//     public ErrorPageFilter errorPageFilter() {
+//         return new ErrorPageFilter();
+//     }
 
-    @Bean
-    public FilterRegistrationBean<ErrorPageFilter> disableErrorPageFilter(ErrorPageFilter filter) {
-        FilterRegistrationBean<ErrorPageFilter> frb = new FilterRegistrationBean<>(filter);
-        frb.setEnabled(false); // keep disabled if you want to disable it
-        return frb;
-    }
-}
+//     @Bean
+//     public FilterRegistrationBean<ErrorPageFilter> disableErrorPageFilter(ErrorPageFilter filter) {
+//         FilterRegistrationBean<ErrorPageFilter> frb = new FilterRegistrationBean<>(filter);
+//         frb.setEnabled(false); // keep disabled if you want to disable it
+//         return frb;
+//     }
+// }
